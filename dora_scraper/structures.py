eab2a7aa9de211b60cc7588b48dd2b22a8e22e86
ekremezm <you@example.com>
2023-02-26 10:54:28 +0300

    structures path changes

--- dora_scraper/structures.py
+++ dora_scraper/structures.py
@@ -3,45 +3,16 @@ import os
 from dotenv import load_dotenv
 from dataclasses import dataclass
 
-
 @dataclass
 class Paths:
     load_dotenv()
-    ROOT = str(os.getenv("DORA_SCRAPER_ROOT"))
+    ROOT = str(os.getenv("DORA_DATA_ROOT"))
 
     IMAGES_ROOT = os.path.join(ROOT, "images/")
     DATA_ROOT = os.path.join(ROOT, "data.json")
-
-    LINKS_ROOT = os.path.join(ROOT, "links")
 
 @dataclass
 class Currencies:
     LIRA = "TRY"
     DOLLAR = "USD"
     EURO = "EUR"
-
-
-"""
-{
-    "id": str,
-    "brand": str,
-    "category": str,
-    "title": str,
-    "category": str,
-    "url": str,
-    "prices": {
-        "TRY" : {"original" : float, "discounted" : float},
-    },
-    "images": {
-        "thumbnail_images" : [
-            {
-                "path" : str,
-                "url" : str,
-                "platform_url" : str,
-            }
-        ]
-    },
-}
-
-
-"""
