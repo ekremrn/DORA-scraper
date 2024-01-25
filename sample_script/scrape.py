aade5c7ab0b1ffa2cdc07b0da57c36c04451431e
ekremrn <ekremezm@gmail.com>
2024-01-25 08:49:40 +0000

    new version

--- sample_script/scrape.py
+++ sample_script/scrape.py
@@ -3,13 +3,9 @@ import os
 from tqdm import tqdm
 from argparse import ArgumentParser
 
+from dora_scraper.browser import Browser
 from dora_scraper.utils.saver import ProductSaver
-from dora_scraper.utils.browser import get_soup, DRIVER
-from dora_scraper.platforms.trendyol import (
-    extract_category_links,
-    generate_pagination_urls,
-    extract_products_from_category_page,
-)
+from dora_scraper.platforms import trendyol as trd
 
 parser = ArgumentParser()
 parser.add_argument("-p", "--path", type=str, default="DATA/")
@@ -18,18 +14,17 @@ parser.add_argument("-u", "--platform_url", type=str, required=True)
 parser.add_argument("-c", "--categories", nargs="+", required=True)
 opt = parser.parse_args()
 
+BROWSER = Browser()
 JSONPATH = os.path.join(opt.path, "{}.json".format(opt.platform_name))
 SAVER = ProductSaver(opt.path, opt.platform_name)
 
-soup = get_soup(opt.platform_url)
-category_links = extract_category_links(soup, opt.categories)
+soup = BROWSER.get_soup(opt.platform_url)
+category_links = trd.extract_category_links(soup, opt.categories)
 
 progress = tqdm(category_links, desc=opt.platform_name, ncols=100, colour="green")
 for category_link in progress:
-    links = generate_pagination_urls(category_link)
+    links = trd.generate_pagination_urls(category_link)
     for link in links:
-        soup = get_soup(link)
-        products = extract_products_from_category_page(soup)
+        soup = BROWSER.get_soup(link)
+        products = trd.extract_products_from_category_page(soup)
         SAVER.product_list(products)
-
-DRIVER.quit()
--- setup.py
+++ setup.py
@@ -2,16 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="dora_scraper",
-    version="0.1.0",
+    version="0.1.1",
     description="Scraper of Dora Platform",
     url="https://github.com/ekremrn/DORA-scraper",
     long_description_content_type="text/markdown",
     author="ekremrn",
     packages=find_packages(),
-    install_requires=[
-        "pandas",
-        "beautifulsoup4",
-        "selenium"
-    ],
+    install_requires=["pandas", "beautifulsoup4", "selenium", "tqdm"],
 )
-
