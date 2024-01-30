3474be7b2a54cdb76e84b17d886e65882f1fde3a
ekremrn <ekremezm@gmail.com>
2024-01-30 12:38:26 +0000

    bugs bugs bugs

--- dora_scraper/browser.py
+++ dora_scraper/browser.py
@@ -13,7 +13,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 def create_driver():
     options = ChromeOptions()
     options.add_argument("--headless")
-    options.add_argument("--incognito")
     options.add_argument("--ignore-ssl-errors=yes")
     options.add_argument("--ignore-certificate-errors")
     return Chrome(service=Service(ChromeDriverManager().install()), options=options)
@@ -21,23 +20,26 @@ def create_driver():
 
 class Browser:
     def __init__(self):
-        self.__DRIVER = create_driver()
+        self._DRIVER = create_driver()
         self.__DELAY = 2
 
     def get_soup(self, url: str) -> BeautifulSoup:
 
         try:
-            self.__DRIVER.get(url)
+            self._DRIVER.get(url)
+            sleep(self.__DELAY)
+            return BeautifulSoup(self._DRIVER.page_source, "html.parser")
+
         except NoSuchWindowException:
             logging.error("Driver: NoSuchWindowException")
-            del self.__DRIVER
-            self.__DRIVER = create_driver()
+            del self._DRIVER
             sleep(self.__DELAY)
+            self._DRIVER = create_driver()
             return self.get_soup(url)
+
         except Exception as e:
             logging.error(e)
-            return None
-
-        sleep(self.__DELAY)
-        soup = BeautifulSoup(self.__DRIVER.page_source, "html.parser")
-        return soup
+            del self._DRIVER
+            sleep(self.__DELAY)
+            self._DRIVER = create_driver()
+            return self.get_soup(url)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="dora_scraper",
-    version="0.1.1",
+    version="0.1.2",
     description="Scraper of Dora Platform",
     url="https://github.com/ekremrn/DORA-scraper",
     long_description_content_type="text/markdown",
